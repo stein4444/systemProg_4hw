9cc46b10d26e8765bcd35c16fcc48393767a5d05
stein4444 <eysmont.vitya@gmail.com>
2021-03-03 11:05:02 +0200

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System.IO;
+using System.Threading;
+using System.Windows;
+using System.Windows.Forms;
+//using System.Windows.Forms;
+namespace systemProg_hw4
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public SortingItem sorting { get; set; }
+        public MainWindow()
+        {
+            InitializeComponent();
+            sorting = new SortingItem();
+            this.DataContext = sorting;
+        }
+
+        private void btnFrom_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new FolderBrowserDialog())
+            {
+                DialogResult result = dialog.ShowDialog();
+                tbFrom.Text = dialog.SelectedPath;
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (tbFrom.Text == null)
+                return;
+            string folder = tbFrom.Text;
+            string[] txtFiles = Directory.GetFiles(folder, "*.txt");
+            lsFiles.ItemsSource = txtFiles;
+            SortLines(txtFiles);
+        }
+
+        private void SortLines(string[] el)
+        {
+            Thread[] threads = new Thread[el.Length];
+            ParameterizedThreadStart start = new ParameterizedThreadStart(sorting.Sorting);
+            for (int i = 0; i < el.Length; i++)
+            {
+                threads[i] = new Thread(start);
+                threads[i].Start(el[i]);
+            }
+        }
+    }
+}
---
+++ SortingItem.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text.RegularExpressions;
+
+namespace systemProg_hw4
+{
+    public class SortingItem : INotifyPropertyChanged
+    {
+        public int words { get; set; }
+        public int lines { get; set; }
+        public int punctuation { get; set; }
+
+        public int Words
+        {
+            get => words;
+            set
+            {
+                words = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Lines
+        {
+            get => lines;
+            set
+            {
+                lines = value;
+                OnPropertyChanged();
+            }
+        }
+        public int Punctuation
+        {
+            get => punctuation;
+            set
+            {
+                punctuation = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public void Sorting(object obj)
+        {
+            string el = (string)obj;
+            string line;
+            int count = 0;
+            using (StreamReader reader = File.OpenText(el))
+            {
+                lock (this)
+                {
+                    while ((line = reader.ReadLine()) != null)
+                    {
+                        count++;
+                        Lines = count;
+                    }
+                    CountingWords(reader, el);
+                    CountingSymbols(reader, el);
+                }
+
+            }
+
+        }
+        public void CountingWords(StreamReader st, string el)
+        {
+            int count = 0;
+            using (st = File.OpenText(el))
+            {
+
+                string line;
+                
+                    while ((line = st.ReadLine()) != null)
+                    {
+                    lock (this)
+                    {
+                        String[] words = line.Split('.', '?', '!', ' ', ';', ':', ',', '+', '–', '…', '"', '\'', '«', '»', '(', ')', '{', '}', '[', ']', '<', '>', '/');
+
+                        count = count + words.Length;
+                        Words = count;
+                    }
+                    }
+            }
+        }
+        public void CountingSymbols(StreamReader st, string el)
+        {
+            int count = 0;
+            var text = new char[] { '.', '?', '!', ' ', ';', ':', ',', '+', '–', '…','"', '\'', '«','»','(', ')', '{', '}', '[', ']',  '<', '>',  '/' };
+            using (st = File.OpenText(el))
+            {
+
+                string line;
+
+                while ((line = st.ReadLine()) != null)
+                {
+                    for (int i = 0; i < text.Length; i++)
+                    {
+                        if (line.Contains(text[i]))
+
+                            lock (this)
+                            {
+                                count++;
+                                Punctuation = count;
+                            }
+
+                    }
+                }
+            }
+        }
+
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
